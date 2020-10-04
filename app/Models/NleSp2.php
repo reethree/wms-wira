8eb3ca91ee753a74dd4a39d667b15ec81ba0c2a1
Reza R Ramadhan <reethree269@gmail.com>
2020-10-04 12:43:05 +0700

    Update

---
+++ app/Models/NleSp2.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NleSp2 extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'nle_sp2';
+    protected $primaryKey = 'id';
+
+}
