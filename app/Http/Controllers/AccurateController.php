4d964c12e908679647f0dd1d173fb257ffc61dd9
Reza R Ramadhan <reethree269@gmail.com>
2021-06-29 12:24:21 +0700

    Update

--- app/Http/Controllers/AccurateController.php
+++ app/Http/Controllers/AccurateController.php
@@ -53,7 +53,7 @@ class AccurateController extends Controller
             $sign = $this->accurate->__getSign(array('id' => 315240));
 
             $response = $this->accurate->getSession('', $access_token);
-
+            dd($response);
             if($response['is_error']) {
 //                return back()->with('error', $response['message']['d']);
                 echo "<div style='padding:20px;'><h1>".$response['message']['d']."</h1></div>";
